1e69dfa0bfe6714c7628196d5efc2629520efc0e
alsghks <dbjenjoy@gmail.com>
2023-06-09 12:46:02 +0900

    20230609
    20230609

---
+++ Zombie-Survival/Assets/01.Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; // 싱글턴을 할당할 전역 변수
+    public bool isGameover = false; // 게임오버 상태
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
